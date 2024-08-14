90dfd2b06b701cc8cd5597a8f5ba99c3233d1e3c
bymse <egorstrelov@gmail.com>
2024-08-14 20:37:07 +0300

    Update user use case

--- Identity.Application/Entities/User.cs
+++ Identity.Application/Entities/User.cs
@@ -2,7 +2,11 @@
 
 public record UserId(Guid Value);
 
-public class User
+public record IdpId(string Value); 
+
+public class User(IdpId idpId)
 {
     public UserId Id { get; private set; } = new(Guid.NewGuid());
+    
+    public IdpId IdpId { get; private set; } = idpId;
 }
---
+++ Identity.Application/UseCases/UpdateIdpUserUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Application.Mediator;
+using Identity.Application.Entities;
+using Identity.Infrastructure.Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Application.UseCases;
+
+public record UpdateIdpUserUseCase(string IdpId) : IUseCase<UserId>;
+
+public class UpdateUserUseCaseHandler(DbContext dbContext) : UseCaseHandler<UpdateIdpUserUseCase, UserId>
+{
+    public override async Task<UserId> Handle(UpdateIdpUserUseCase useCase, CancellationToken ct)
+    {
+        var user = await dbContext
+            .Set<User>()
+            .FirstOrDefaultAsync(e => e.IdpId.Value == useCase.IdpId, ct);
+
+        if (user is null)
+        {
+            user = new User(new IdpId(useCase.IdpId));
+            dbContext.Add(user);
+        }
+        
+        await dbContext.SaveChangesAsync(ct);
+    }
+}
