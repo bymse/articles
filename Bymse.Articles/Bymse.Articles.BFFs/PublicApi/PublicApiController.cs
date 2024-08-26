f27e46d0a5482dcf25514bcefa9d8895a3dff092
bymse <egorstrelov@gmail.com>
2024-08-26 23:01:56 +0300

    Save user sources

--- Bymse.Articles/Bymse.Articles.BFFs/PublicApi/PublicApiController.cs
+++ Bymse.Articles/Bymse.Articles.BFFs/PublicApi/PublicApiController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Identity.Integration;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bymse.Articles.BFFs.PublicApi;
 
 [Route("public-api")]
 [ApiController]
-public class PublicApiController : ControllerBase 
+public class PublicApiController : ControllerBase
 {
-    
+    protected IdentityUserId UserId => new(
+        Ulid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier))
+    );
 }
--- Bymse.Articles/Bymse.Articles.BFFs/PublicApi/Sources/SourcesPublicApiController.cs
+++ Bymse.Articles/Bymse.Articles.BFFs/PublicApi/Sources/SourcesPublicApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Mediator;
 using Collector.Application.UseCases.Create;
+using Feeder.Application.UseCases;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bymse.Articles.BFFs.PublicApi.Sources;
@@ -17,6 +18,10 @@ public class SourcesPublicApiController : PublicApiController
     [HttpPost]
     public async Task<UnconfirmedSourceInfo> CreateSource([FromBody] CreateSourceRequest request, CancellationToken ct)
     {
-        return await useCaseExecutor.Execute(new CreateSourceUseCase(request.Title, request.WebPage), ct);
+        var sourceInfo = await useCaseExecutor.Execute(new CreateSourceUseCase(request.Title, request.WebPage), ct);
+        
+        await useCaseExecutor.Execute(new AddUserSourceUseCase(UserId, sourceInfo.Id), ct);
+        
+        return sourceInfo;
     }
 }
