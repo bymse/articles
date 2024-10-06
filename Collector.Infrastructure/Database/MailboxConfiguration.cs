88067b4225e020fee5c91ddba80f5d14fd034b2b
bymse <egorstrelov@gmail.com>
2024-10-06 14:43:59 +0300

    Mailbox configuration and migration

---
+++ Collector.Infrastructure/Database/MailboxConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Collector.Application.Entities;
+using Infrastructure.Ulids;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Collector.Infrastructure.Database;
+
+public class MailboxConfiguration : IEntityTypeConfiguration<Mailbox>
+{
+    public void Configure(EntityTypeBuilder<Mailbox> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder
+            .Property(e => e.Id)
+            .HasConversion<UlidValueConverter>();
+    }
+}
---
+++ Collector.Infrastructure/Migrations/20241006114339_AddMailbox.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Collector.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMailbox : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "mailbox",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "character varying(26)", nullable: false),
+                    uid_validity = table.Column<long>(type: "bigint", nullable: true),
+                    last_uid = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_mailbox", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "mailbox");
+        }
+    }
+}
