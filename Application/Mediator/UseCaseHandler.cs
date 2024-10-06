669944e4656e6afa0b9cd9eabcb9633b3c83bd61
bymse <egorstrelov@gmail.com>
2024-10-06 21:07:05 +0300

    Add PublishEndpoint to mediator handlers

--- Application/Mediator/UseCaseHandler.cs
+++ Application/Mediator/UseCaseHandler.cs
@@ -5,13 +5,36 @@ using MassTransit.Mediator;
 namespace Application.Mediator;
 
 [UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
-public abstract class UseCaseHandler<TUseCase> : MediatorRequestHandler<TUseCase> where TUseCase : class, IUseCase
+public abstract class UseCaseHandler<TUseCase> : IConsumer<TUseCase> where TUseCase : class, IUseCase
 {
+    public async Task Consume(ConsumeContext<TUseCase> context)
+    {
+        PublishEndpoint = context;
+        await Handle(context.Message, context.CancellationToken);
+        PublishEndpoint = null!;
+    }
+
+    protected IPublishEndpoint PublishEndpoint { get; private set; } = null!;
+
+    protected abstract Task Handle(TUseCase request, CancellationToken cancellationToken);
 }
 
 [UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
-public abstract class UseCaseHandler<TUseCase, TResult> : MediatorRequestHandler<TUseCase, TResult>
-    where TUseCase : class, IUseCase<TResult> where TResult : class
+public abstract class UseCaseHandler<TUseCase, TResult> :
+    IConsumer<TUseCase>
+    where TUseCase : class, Request<TResult>
+    where TResult : class
 {
+    public async Task Consume(ConsumeContext<TUseCase> context)
+    {
+        PublishEndpoint = context;
+        var response = await Handle(context.Message, context.CancellationToken).ConfigureAwait(false);
 
+        await context.RespondAsync(response).ConfigureAwait(false);
+        PublishEndpoint = null!;
+    }
+
+    protected IPublishEndpoint PublishEndpoint { get; private set; } = null!;
+
+    protected abstract Task<TResult> Handle(TUseCase request, CancellationToken cancellationToken);
 }
