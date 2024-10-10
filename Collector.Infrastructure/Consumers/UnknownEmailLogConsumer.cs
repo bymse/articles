bc82270c5c3cfeebfa59e649840039824a007cb2
bymse <egorstrelov@gmail.com>
2024-10-10 16:46:30 +0300

    Consumer for unknown emails

---
+++ Collector.Infrastructure/Consumers/UnknownEmailLogConsumer.cs
@@ -0,0 +1,15 @@
+﻿using Collector.Application.Events;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Collector.Infrastructure.Consumers;
+
+public class UnknownEmailLogConsumer(ILogger<UnknownEmailLogConsumer> logger) : IConsumer<UnknownEmailReceivedEvent>
+{
+    public Task Consume(ConsumeContext<UnknownEmailReceivedEvent> context)
+    {
+        logger.LogError("Received unknown email with id {ReceivedEmailId}", context.Message.ReceivedEmailId);
+        
+        return Task.CompletedTask;
+    }
+}
