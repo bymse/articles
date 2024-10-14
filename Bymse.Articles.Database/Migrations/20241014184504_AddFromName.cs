aa374b15af9502d6928a58a82fb1d8e3cc096692
bymse <egorstrelov@gmail.com>
2024-10-14 21:45:56 +0300

    Add from name migration

---
+++ Bymse.Articles.Database/Migrations/20241014184504_AddFromName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bymse.Articles.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFromName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "from_name",
+                table: "received_email",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "from_name",
+                table: "received_email");
+        }
+    }
+}
