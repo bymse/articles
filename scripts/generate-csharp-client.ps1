65d3eca547bc137eda8c3c64e2b68c8d47b9e7f9
bymse <egorstrelov@gmail.com>
2024-10-21 08:10:33 +0300

    Fix openapi generator script

--- scripts/generate-csharp-client.ps1
+++ scripts/generate-csharp-client.ps1
@@ -1,7 +1,7 @@
-dotnet build ../Bymse.Articles/Bymse.Articles.Apis -c Debug
+dotnet build ../Bymse.Articles.Apis -c Debug
 dotnet tool restore
 dotnet swagger tofile `
 --yaml `
---output ../Bymse.Articles/Bymse.Articles.Apis/docs/openapi.yaml `
-../Bymse.Articles/Bymse.Articles.Apis/bin/Debug/net8.0/Bymse.Articles.Apis.dll `
+--output ../Bymse.Articles.Apis/docs/public-api-openapi.yaml `
+../Bymse.Articles.Apis/bin/Debug/net8.0/Bymse.Articles.Apis.dll `
 public-api 
