3a1b9cb112be18998b21bddac9c6dde9a0d2a48b
bymse <egorstrelov@gmail.com>
2024-11-03 20:19:44 +0300

    Generate client

--- Bymse.Articles.PublicApi.Client/PublicApiClient.cs
+++ Bymse.Articles.PublicApi.Client/PublicApiClient.cs
@@ -72,6 +72,84 @@ namespace Bymse.Articles.PublicApi.Client
 
         /// <returns>OK</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual System.Threading.Tasks.Task<ManualProcessingEmailInfoCollection> GetManualProcessingEmailsAsync()
+        {
+            return GetManualProcessingEmailsAsync(System.Threading.CancellationToken.None);
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>OK</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual async System.Threading.Tasks.Task<ManualProcessingEmailInfoCollection> GetManualProcessingEmailsAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+
+                    var urlBuilder_ = new System.Text.StringBuilder();
+                    if (!string.IsNullOrEmpty(_baseUrl)) urlBuilder_.Append(_baseUrl);
+                    // Operation Path: "public-api/manual-processing-emails"
+                    urlBuilder_.Append("public-api/manual-processing-emails");
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.IEnumerable<string>>();
+                        foreach (var item_ in response_.Headers)
+                            headers_[item_.Key] = item_.Value;
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ManualProcessingEmailInfoCollection>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new ApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            return objectResponse_.Object;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+
+        /// <returns>OK</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
         public virtual System.Threading.Tasks.Task<UnconfirmedSourceInfo> CreateSourceAsync(CreateSourceRequest body)
         {
             return CreateSourceAsync(body, System.Threading.CancellationToken.None);
@@ -442,6 +520,58 @@ namespace Bymse.Articles.PublicApi.Client
 
         [Newtonsoft.Json.JsonProperty("webPage", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public System.Uri WebPage { get; set; }
+
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
+    public partial class ManualProcessingEmailInfo
+    {
+        [Newtonsoft.Json.JsonProperty("receivedEmailId", Required = Newtonsoft.Json.Required.Always)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        public string ReceivedEmailId { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("type", Required = Newtonsoft.Json.Required.Always)]
+        [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
+        [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+        public ManualProcessingEmailType Type { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("toEmail", Required = Newtonsoft.Json.Required.AllowNull)]
+        public string ToEmail { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("fromEmail", Required = Newtonsoft.Json.Required.AllowNull)]
+        public string FromEmail { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("fromName", Required = Newtonsoft.Json.Required.AllowNull)]
+        public string FromName { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("subject", Required = Newtonsoft.Json.Required.AllowNull)]
+        public string Subject { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("textBody", Required = Newtonsoft.Json.Required.AllowNull)]
+        public string TextBody { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("htmlBody", Required = Newtonsoft.Json.Required.AllowNull)]
+        public string HtmlBody { get; set; }
+
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
+    public partial class ManualProcessingEmailInfoCollection
+    {
+        [Newtonsoft.Json.JsonProperty("items", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.Collections.Generic.ICollection<ManualProcessingEmailInfo> Items { get; set; }
+
+    }
+
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
+    public enum ManualProcessingEmailType
+    {
+
+        [System.Runtime.Serialization.EnumMember(Value = @"UnknownEmailType")]
+        UnknownEmailType = 0,
+
+        [System.Runtime.Serialization.EnumMember(Value = @"ConfirmSubscription")]
+        ConfirmSubscription = 1,
 
     }
 
