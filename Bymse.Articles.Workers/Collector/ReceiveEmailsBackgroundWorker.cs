898652854da77ceaab65c66bde0c6ba762bb6e65
bymse <egorstrelov@gmail.com>
2024-11-05 08:32:48 +0300

    Error handling in ReceiveEmailsBackgroundWorker

--- Bymse.Articles.Workers/Collector/ReceiveEmailsBackgroundWorker.cs
+++ Bymse.Articles.Workers/Collector/ReceiveEmailsBackgroundWorker.cs
@@ -2,13 +2,26 @@
 
 namespace Bymse.Articles.Workers.Collector;
 
-public class ReceiveEmailsBackgroundWorker(IServiceProvider serviceProvider) : BackgroundService
+public class ReceiveEmailsBackgroundWorker(
+    IServiceProvider serviceProvider,
+    ILogger<ReceiveEmailsBackgroundWorker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        using var scope = serviceProvider.CreateScope();
-        var handler = scope.ServiceProvider.GetRequiredService<ReceiveEmailsHandler>();
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                using var scope = serviceProvider.CreateScope();
+                var handler = scope.ServiceProvider.GetRequiredService<ReceiveEmailsHandler>();
 
-        await handler.Handle(stoppingToken);
+                await handler.Handle(stoppingToken);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error occurred while handling emails");
+                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+            }
+        }
     }
 }
