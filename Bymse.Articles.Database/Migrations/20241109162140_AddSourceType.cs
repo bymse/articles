b74c058fdd062eefa466dc425269ca224e892e1c
bymse <egorstrelov@gmail.com>
2024-11-09 19:22:07 +0300

    Source type migration

---
+++ Bymse.Articles.Database/Migrations/20241109162140_AddSourceType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bymse.Articles.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSourceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "sources",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "sources");
+        }
+    }
+}
